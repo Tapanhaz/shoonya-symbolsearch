996ec5af44d5b3bb7d316595754318daf3674fdf
Tapanhaz <91151267+Tapanhaz@users.noreply.github.com>
2023-08-07 00:01:13 +0530

    Update check_symbols.py

--- Examples/check_symbols.py
+++ Examples/check_symbols.py
@@ -1,3 +1,18 @@
+'''
+    The nse workday library can be used to get the holidays / workday's list
+as per nse trading holidays. It have some more functions.
+I am not making that package open source for some reason. 
+If using win X64, python 3.10 / 3.11 the library can be installed by pip install nse-workday
+more info on that here : https://github.com/Tapanhaz/nse_workday
+
+    Here that library is used to get the nearest expiry of banknifty.( The get_weekdays function will return
+the given required weekday within the given period. if the calculated weekday is a holiday, it will return nearest 
+previous workday.
+
+    This method should help to get correct symbolmaster. If not win X64, python 3.10 / 3.11 user or
+do not like to use closed source libraries, please check the other solution.
+'''
+
 from symbolsearch import SearchScrip
 from nse_workday import get_weekdays
 from datetime import datetime, timedelta
